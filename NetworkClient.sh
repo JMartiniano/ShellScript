079f6fd985662892205431e1605624d75915b64d
JMartiniano <jose.martiniano01@gmail.com>
2021-10-10 10:57:30 -0300

    Get IP

--- NetworkClient.sh
+++ NetworkClient.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Começando o data.json
-echo -e "{ \n	'ips': [" >> data.json
-
 # Pegando o ip da rede
 ip="$(ip addr | grep 'enp' | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.' | head -n 1)"
 
